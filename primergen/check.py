868fc23928381f3d2d3c36095732c805037d0359
Sriram Sami <frizensami@gmail.com>
2021-11-27 21:50:31 -0800

    Add overall primer validity check and tests

--- primergen/check.py
+++ primergen/check.py
@@ -13,12 +13,49 @@ MIN_CG_CONTENT = 45
 MAX_CG_CONTENT = 55
 
 from Bio.SeqUtils import GC
+import editdistance
+import itertools
+import functools
 
 
-def are_primers_valid(primers: [str]) -> bool:
+def are_primers_valid(primers):
+    """
+    Runs
+    """
+    # Run the easy checks first
+    easy_valids = map(is_len_gc_valid, primers)
+    is_all_valid_easy = all(easy_valids)
+    if not is_all_valid_easy:
+        print(f"Either wrong length or GC failure")
+        return False
+
+    # Run the n-choose-2 valids now
+    for (p1, p2) in itertools.combinations(primers, 2):
+        if not is_primer_pair_valid(p1, p2):
+            print(f"{p1} and {p2} are not {MIN_EDIT_DISTANCE} edit distance apart!")
+            return False
+
+    return True
+
+
+def get_valid_primers(primers):
     pass
+
+
+def is_right_length(primer) -> bool:
+    return len(primer) == 20
 
 
 def is_gc_valid(primer) -> bool:
     gc_percentage = GC(primer)
     return MIN_CG_CONTENT <= gc_percentage and gc_percentage <= MAX_CG_CONTENT
+
+
+def is_len_gc_valid(primer) -> bool:
+    return is_right_length(primer) and is_gc_valid(primer)
+
+
+@functools.lru_cache(maxsize=None)
+def is_primer_pair_valid(p1, p2):
+    dist = editdistance.eval(p1, p2)
+    return dist > MIN_EDIT_DISTANCE
--- tests/unit/primergen/check.py
+++ tests/unit/primergen/check.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from primergen.check import is_gc_valid
+from primergen.check import is_right_length, is_gc_valid, are_primers_valid
 from Bio.Seq import Seq
 import unittest
 
@@ -9,6 +9,37 @@ class TestCheckMethods(unittest.TestCase):
     def test_gc_content(self):
         s = Seq("ATGC")
         self.assertTrue(is_gc_valid(s))
+
+    def test_gc_content_toolittle(self):
+        s = Seq("ATTC")
+        self.assertFalse(is_gc_valid(s))
+
+    def test_gc_content_toomuch(self):
+        s = Seq("AGGC")
+        self.assertFalse(is_gc_valid(s))
+
+    def test_length_correct(self):
+        s = Seq("ATGCA" * 4)
+        self.assertTrue(is_right_length(s))
+
+    def test_length_toolong(self):
+        s = Seq("ATGCA" * 5)
+        self.assertFalse(is_right_length(s))
+
+    def test_length_tooshort(self):
+        s = Seq("ATGCA" * 3)
+        self.assertFalse(is_right_length(s))
+
+    def test_all_primers_ok(self):
+        s1 = Seq("ATGC" * 5)
+        s2 = Seq("GCTA" * 5)
+        s3 = Seq("CGTA" * 5)
+        self.assertTrue(are_primers_valid([s1, s2, s3]))
+
+    def test_all_primers_ok_two_identical_fail(self):
+        s1 = Seq("ATGC" * 5)
+        s2 = Seq("ATGC" * 5)
+        self.assertFalse(are_primers_valid([s1, s2]))
 
 
 if __name__ == "__main__":
